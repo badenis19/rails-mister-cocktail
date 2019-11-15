c7299192c13a20f653ab3aa88ee5eb889f7d1264
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-11-15 15:06:33 +0000

    still working on it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # to have access to the cocktails ID on which I will associate my dose
+  resources :cocktails, only: [:index, :new, :show, :edit, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:new, :destroy]
+
 end
+
+
+
